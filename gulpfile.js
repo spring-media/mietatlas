80a42d953969f2d1ebe8befdcd9ab443efd5faf9
Mucha <vmucha@XIN852646.asv.local>
2016-11-17 10:56:03 +0100

    add vis

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ gulp.task('styles', function () {
     return gulp.src(['app/styles/main.css'])
         .pipe($.autoprefixer('last 1 version'))
         .pipe(gulp.dest('app/styles'))
+        .pipe($.livereload())
         .pipe($.size());
 });
 
